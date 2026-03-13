e9a66eeae089029ef417b09b09c4e6238a66d1d6
Eric Fortis <efortis@gmail.com>
2026-03-13 13:40:22 -0400

    refactor

--- generate-fixtures.sh
+++ generate-fixtures.sh
@@ -7,8 +7,8 @@ docker build -t mediasnacks-test .
 cat << EOF
 To generate fixtures:
 	node --test
-	cp /tmp/avif-*/lenna.avif /workspace/src/fixtures/
-	cp /tmp/edgespic-*/edgespic/*.png /workspace/src/fixtures/edgespic/
+	cp /tmp/avif*/lenna.avif /workspace/src/fixtures/
+	cp /tmp/edgespic*/edgespic/*.png /workspace/src/fixtures/edgespic/
 EOF
 
 docker run --rm -it \
