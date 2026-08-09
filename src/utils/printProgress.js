b2060459168892510c4bb7c4d54190f3170febf6
Eric Fortis <efortis@gmail.com>
2026-08-09 01:42:23 -0400

    refactor

--- src/utils/printProgress.js
+++ src/utils/printProgress.js
@@ -27,7 +27,8 @@ function progressBar(progress, width = 44) {
 	const nFull = (width * progress) | 0
 	const fPartial = (width * progress) - nFull
 	const nRemaining = width - nFull
+
 	const partials = ' ▏▎▍▌▋▊▉'
-	const partial = partials[Math.min(partials.length * fPartial | 0, partials.length - 1)]
+	const partial = partials.at(partials.length * fPartial)
 	return '█'.repeat(nFull) + partial + '•'.repeat(nRemaining)
 }
